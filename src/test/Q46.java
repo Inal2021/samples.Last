309e8a85cf74373ee718e6784a7d4990b0c0fd8f
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2021-10-06 08:56:45 +0200

    initial commitV3

---
+++ src/test/Q46.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Q46 {
+
+    public static void main(String[] args) {
+
+       /* Yazılan iki String'den ikincisi, birincisinin son harflerine eşitse, true, diğer durumda false dönderen bir program yazınız.
+
+        Örnekler:
+        kontrolEt("abc", "bc") ➞ true
+        kontrolEt("abc", "d") ➞ false
+        kontrolEt("samurai", "zi") ➞ false
+        kontrolEt("feminine", "nine") ➞ true
+        kontrolEt("convention", "tio") ➞ false    */
+
+    }
+}
---
+++ src/test/Q48.java
@@ -0,0 +1,25 @@
+package test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Q48 {
+
+    private static LinkedList<Object> ArrayConcatUtils;
+
+    public static void main(String[] args) {
+
+    //    İki farklı Array'i birleştiren bir program yazınız.
+
+        int[] first={1,3,5,7};
+        int[] second={2,4,6,8};
+
+        int[] both= new int[];
+
+
+
+
+    }
+
+
+}
